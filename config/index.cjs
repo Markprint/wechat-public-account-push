3b36c7f4adca5ae82ca18427d7c1702d0f3b4faf
Markprint <34962143+Markprint@users.noreply.github.com>
2022-11-10 10:13:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,7 +40,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-10-26' },
+        { keyword: 'love_day', date: '2022-09-20' },
         
       ],
     },
